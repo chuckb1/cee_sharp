68a325c359cc0e98d1984e096d6fee95d1e59067
Chuck Breter <chuckb0384@aim.com>
2022-06-08 12:35:21 -0400

    omni

---
+++ w1/d3/dailyalgo6-8.js
@@ -0,0 +1,11 @@
+// Given a value, return true or false whether that value is in your list
+contains(val){
+    // your code here
+}
+
+// If you didn't already come up with the solution recursively, try that now! Or, if you did solve it recursively, how might you do it iteratively? 
+
+// Remove and return the last value in your list
+removeBack(){
+    // your code here
+}
