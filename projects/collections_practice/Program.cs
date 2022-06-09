4f3c0cc6c42b343a30003c86273ffd3fcc0f3f44
Chuck Breter <chuckb0384@aim.com>
2022-06-09 02:23:40 -0400

    collections_practice

---
+++ projects/collections_practice/Program.cs
@@ -0,0 +1,71 @@
+﻿// Create an array to hold integer values 0 through 9
+// Create an array of the names "Tim", "Martin", "Nikki", & "Sara"
+// Create an array of length 10 that alternates between true and false values, starting with true
+
+// int[] array1 =  {0,1,2,3,4,5,6,7,8,9};
+// string[] array2 = new string[] {"tim", "Martin", "Nikki", "Sara"};
+// bool[] array3 = {true, false, true, false, true, false, true, false, true, false};
+
+
+// Create a list of ice cream flavors that holds at least 5 different flavors (feel free to add more than 5!)
+// Output the length of this list after building it
+// Output the third flavor in the list, then remove this value
+// Output the new length of the list (It should just be one fewer!)
+
+// This is the list from the assignment that has you change some of the flavors
+// List<string> IceCreamFlavors = new List<string>();
+// IceCreamFlavors.Add("Blue Moon");
+// IceCreamFlavors.Add("Reece Cup");
+// IceCreamFlavors.Add("Chocolate");
+// IceCreamFlavors.Add("Vanilla");
+// IceCreamFlavors.Add("Oreo");
+// IceCreamFlavors.Add("Strawberry");
+// IceCreamFlavors.Add("Neapolitan");
+// IceCreamFlavors.Add("Banana");
+// IceCreamFlavors.Add("Sherbert");
+// IceCreamFlavors.Add("Rocky Road");
+// Console.WriteLine(IceCreamFlavors.Count);
+// Console.WriteLine(IceCreamFlavors[2]);
+// IceCreamFlavors.Remove(IceCreamFlavors[2]);
+// Console.WriteLine(IceCreamFlavors.Count);
+
+
+
+
+
+// I made this list longer
+string[] People = {"Tim", "Martin", "Nikki", "Sara", "Logan", "Dad", "Jeff"};
+
+// I changed the list back to normal to make the last assignment easier for me to understand
+List<string> IceCreamFlavors = new List<string>();
+IceCreamFlavors.Add("Blue Moon");
+IceCreamFlavors.Add("Reece Cup");
+IceCreamFlavors.Add("Chocolate");
+IceCreamFlavors.Add("Vanilla");
+IceCreamFlavors.Add("Oreo");
+IceCreamFlavors.Add("Strawberry");
+IceCreamFlavors.Add("Neapolitan");
+IceCreamFlavors.Add("Banana");
+IceCreamFlavors.Add("Sherbert");
+IceCreamFlavors.Add("Rocky Road");
+
+// Create a dictionary that will store both string keys as well as string values
+// Add key/value pairs to this dictionary where:
+// each key is a name from your names array
+// each value is a randomly elected flavor from your flavors list.
+// Loop through the dictionary and print out each user's name and their associated ice cream flavor
+Dictionary<string,string> FavoriteIceCream = new Dictionary<string, string>();
+foreach (string Person in People) {
+    Random rnd = new Random();
+    int num = rnd.Next(9);
+    string FavCream = IceCreamFlavors[num];
+    FavoriteIceCream.Add(Person, FavCream);
+}
+foreach (KeyValuePair<string, string> entry in FavoriteIceCream) {
+    Console.WriteLine(entry.Key + "-" + entry.Value);
+} 
+
+
+
+
+
---
+++ w1/d1/practiced1/Program.cs
@@ -0,0 +1,39 @@
+﻿// namespace HelloWorld
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("What is your name?");
+//             var name = Console.ReadLine();
+//             var currentDate = DateTime.Now;
+//             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+//             Console.Write($"{Environment.NewLine}Press any key to exit...");
+//             Console.ReadKey(true);    
+//             // Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+// Random rand = new Random();
+// for(int val = 0; val < 10; val++)
+// {
+//     //Prints the next random value between 2 and 8
+//     Console.WriteLine(rand.Next(2,8));
+// }
+Dictionary<string,string> profile = new Dictionary<string,string>();
+//Almost all the methods that exists with Lists are the same with Dictionaries
+profile.Add("Name", "Speros");
+profile.Add("Language", "PHP");
+profile.Add("Location", "Greece");
+Console.WriteLine("Instructor Profile");
+Console.WriteLine("Name - " + profile["Name"]);
+Console.WriteLine("From - " + profile["Location"]);
+Console.WriteLine("Favorite Language - " + profile["Language"]);
+
+
+foreach (KeyValuePair<string,string> entry in profile)
+{
+    Console.WriteLine(entry.Key + " - " + entry.Value);
+}
+
